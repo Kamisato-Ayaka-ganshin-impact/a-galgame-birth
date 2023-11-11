2e9f724bfe353ef1dfc77e46ecdb0aa6d09c32d0
ColdSnow-byte <3237451511@qq.com>
2023-11-11 15:23:22 +0800

    11

--- js/total-index.js
+++ js/total-index.js
@@ -6,3 +6,8 @@ function hideDescription(gameId) {
     document.getElementById(gameId + "-description").style.display = "none";
 }
 //<!--实现将鼠标放在图片上就出现图片的说明-->
+function goToLink(gameId) {
+    var link = document.getElementById(gameId + "-link").href;
+    window.location.href = link;
+}
+//超链接
