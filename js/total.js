16b14ca2c93f005990292a280a1d0c414f571087
asdasaffafafsdf <3106636586@qq.com>
2023-11-18 23:19:46 +0800

    图片整理

--- js/total.js
+++ js/total.js
@@ -1,14 +1,18 @@
 var slideIndex = [1, 1];
 
 function plusSlides(n, index) {
-  showSlides(slideIndex[index] += n, index);
+  showSlides((slideIndex[index] += n), index);
 }
 
 function showSlides(n, index) {
   var i;
   var slides = document.querySelectorAll('.images')[index].getElementsByClassName("mySlides");
-  if (n > slides.length) { slideIndex[index] = 1 }
-  if (n < 1) { slideIndex[index] = slides.length }
+  if (n > slides.length) {
+    slideIndex[index] = 1;
+  }
+  if (n < 1) {
+    slideIndex[index] = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -16,6 +20,6 @@ function showSlides(n, index) {
 }
 
 setInterval(function () {
-  plusSlides(1, 0);
-  plusSlides(1, 1);
-}, 2000);
+  plusSlides(1, 0); // 第一个轮播图
+  plusSlides(1, 1); // 第二个轮播图
+}, 1000);
